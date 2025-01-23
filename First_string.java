362b1ff27e3291f53aa4919757a9dec3b93a3a94
Siddhant Choubey <123854867+sid9301@users.noreply.github.com>
2025-01-23 02:44:11 +0000

    this is string

---
+++ First_string.java
@@ -0,0 +1,17 @@
+public class First_string {
+    public static void main(String args[]){
+        String s = "The string program ";
+        System.out.println(s);
+        // string are two types 
+        /*
+         * literal string 
+           * String str = "this is literal string type";
+           * not new object is create if string pool exists
+           * this is static memory
+           * 
+         * using new keyword
+          *String str1 = new String("new object is created");
+          * this is head memory  
+         */
+    }
+}
